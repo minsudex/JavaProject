d17107581d28c847a2ef48cb4ced8e052a495897
minsudex <minsuf4@naver.com>
2023-12-28 13:43:48 +0900

    코드 생성

---
+++ BoardRepository.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class BoardRepository {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---------------------------------------------------------");
+        System.out.println("1. 회원가입 | 2. 로그인 ");
+    }
+}
