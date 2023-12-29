9db593a5405793ba7d9fea76d1185802dd3c20f4
minsudex <minsuf4@naver.com>
2023-12-29 10:34:47 +0900

    Changes

--- BoardRepository.java
+++ BoardRepository.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class BoardRepository {
     public static void main(String[] args) {
+        BoardRepository boardRepository = new BoardRepository();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("---------------------------------------------------------");
-        System.out.println("1. 회원가입 | 2. 로그인 ");
+        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.println("1.회원가입 | 2.로그인 | 3.회원목록 | 4.회원수정 | 5.회원탈퇴 | 6.로그아웃 | 7.게시판메뉴 | 0.종료");
+        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.print("선택> ");
     }
-}
+    }
